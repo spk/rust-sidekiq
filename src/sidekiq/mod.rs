5d6547e8ee7f7d364faeb494ef4a51401080666b
Laurent Arnoud <laurent@spkdev.net>
2017-04-16 12:11:36 +0200

    Add REDIS_URL_ENV const

--- src/sidekiq/mod.rs
+++ src/sidekiq/mod.rs
@@ -14,6 +14,7 @@ use serde_json;
 use r2d2_redis::RedisConnectionManager;
 use r2d2::{Config, Pool, PooledConnection, GetTimeout, InitializationError};
 
+const REDIS_URL_ENV: &'static str = "REDIS_URL";
 const REDIS_URL_DEFAULT: &'static str = "redis://127.0.0.1/";
 pub type RedisPooledConnection = PooledConnection<RedisConnectionManager>;
 pub type RedisPool = Pool<RedisConnectionManager>;
@@ -32,7 +33,7 @@ enum ErrorKind {
 
 pub fn create_redis_pool() -> Result<RedisPool, ClientError> {
     let config = Config::builder().build();
-    let redis_url = &env::var("REDIS_URL").unwrap_or_else(|_| REDIS_URL_DEFAULT.to_owned());
+    let redis_url = &env::var(&REDIS_URL_ENV.to_owned()).unwrap_or_else(|_| REDIS_URL_DEFAULT.to_owned());
     let url = redis::parse_redis_url(redis_url).unwrap();
     let manager = RedisConnectionManager::new(url).unwrap();
     Pool::new(config, manager).map_err(|err| ClientError { kind: ErrorKind::PoolInit(err) })
