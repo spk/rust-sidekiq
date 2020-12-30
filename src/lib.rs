fe5c838606f2f1ab08154a28d1f832879b42e5f8
Joel Johnson <johnson.joel.b@gmail.com>
2020-12-30 12:18:34 -0500

    Make ClientError public

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,6 @@ extern crate serde_json;
 
 mod sidekiq;
 pub use crate::sidekiq::{
-    create_redis_pool, Client, ClientOpts, Job, JobOpts, RedisPool, RedisPooledConnection,
+    create_redis_pool, Client, ClientError, ClientOpts, Job, JobOpts, RedisPool, RedisPooledConnection,
 };
 pub use serde_json::value::Value;
